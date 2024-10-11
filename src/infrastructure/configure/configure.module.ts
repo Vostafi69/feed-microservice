82f55ba5e75e938567f0edc962cb3cf39ef63901
Alexander <117897362+Vostafi69@users.noreply.github.com>
2024-10-11 14:25:49 +0300

    feat: В файл конфигурации добавлен throttle

--- src/infrastructure/configure/configure.module.ts
+++ src/infrastructure/configure/configure.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
+    }),
+    ThrottlerModule.forRootAsync({
+      inject: [ConfigService],
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => [
+        {
+          ttl: config.get<number>('THROTTLE_TTL'),
+          limit: config.get<number>('THROTTLE_LIMIT'),
+        },
+      ],
     }),
   ],
 })
