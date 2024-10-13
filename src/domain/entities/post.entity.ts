de1979ac0977b30c30f1caab4e505ce3c60475ba
Alexander <117897362+Vostafi69@users.noreply.github.com>
2024-10-13 23:00:25 +0300

    refactor: Переименованы файлы сущностей

--- src/domain/entities/post.entity.ts
+++ src/domain/entities/post.entity.ts
@@ -2,7 +2,7 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 import { BaseEntity } from './base-entity';
 
 @Entity({ name: 'post' })
-export class Post extends BaseEntity {
+export class PostEntity extends BaseEntity {
   @PrimaryGeneratedColumn('increment', { name: 'id' })
   id: number;
 
