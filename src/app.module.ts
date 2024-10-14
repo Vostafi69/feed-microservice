4eb02003b7295c54ef5ab2c4daf85541af853c17
Alexander <117897362+Vostafi69@users.noreply.github.com>
2024-10-14 17:54:01 +0300

    feat: Пост модуль подключен к основному приложению

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigureModule } from './infrastructure/configure/configure.module';
 import { DatabaseModule } from './infrastructure/database/database.module';
+import { PostModule } from './api/post/post.module';
 
 @Module({
-  imports: [ConfigureModule, DatabaseModule],
+  imports: [ConfigureModule, DatabaseModule, PostModule],
   controllers: [],
   providers: [],
 })
