b39bfdd6dc710763afb6514d05f5ad5c1f72b268
Alexander <117897362+Vostafi69@users.noreply.github.com>
2024-10-14 18:45:29 +0300

    fix: Исправлен импорт

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigureModule } from './infrastructure/configure/configure.module';
 import { DatabaseModule } from './infrastructure/database/database.module';
-import { PostModule } from './api/post/post.module';
+import { PostModule } from './api/v1/post/post.module';
 
 @Module({
   imports: [ConfigureModule, DatabaseModule, PostModule],
