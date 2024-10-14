807d1744b6b0201e5569bbef0e9d77d8e74d2f39
Alexander <117897362+Vostafi69@users.noreply.github.com>
2024-10-14 18:46:15 +0300

    refactor: Изменена базова сущность и сущность поста

--- src/domain/entities/post.entity.ts
+++ src/domain/entities/post.entity.ts
@@ -1,11 +1,8 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity } from 'typeorm';
 import { BaseEntity } from './base.entity';
 
 @Entity({ name: 'post' })
 export class PostEntity extends BaseEntity {
-  @PrimaryGeneratedColumn('increment', { name: 'id' })
-  id: number;
-
   @Column({ name: 'title' })
   title: string;
 
