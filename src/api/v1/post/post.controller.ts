f406c63cb05e14bbd4c4ef3665262f19a64a3689
Alexander <117897362+Vostafi69@users.noreply.github.com>
2024-10-19 22:54:51 +0300

    feat: Команда создания поста добавлена в контроллер

--- src/api/v1/post/post.controller.ts
+++ src/api/v1/post/post.controller.ts
@@ -1,14 +1,16 @@
 import {
+  Body,
   Controller,
   Get,
   HttpCode,
   HttpStatus,
   Param,
   ParseIntPipe,
+  Post,
   Query,
   Version,
 } from '@nestjs/common';
-import { QueryBus } from '@nestjs/cqrs';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { GetPostsQuery } from './queries/impl/get-posts.query';
 import { PageOptionsDto } from '../dto/page-options.dto';
 import { PageDto } from '../dto/page.dto';
@@ -16,12 +18,17 @@ import { ApiTags } from '@nestjs/swagger';
 import { PostDto } from './dto/post.dto';
 import { ApiPaginatedResponse } from 'src/main/config/docs/decorators/api-paginated-response';
 import { GetPostByIdQuery } from './queries/impl/get-post-byId.query';
+import { AddPostDto } from '../dto/add-post.dto';
+import { AddPostCommand } from './commands/impl/add-post.command';
 
 Version('1');
 @Controller('posts')
 @ApiTags('posts')
 export class PostController {
-  constructor(private readonly _queryBus: QueryBus) {}
+  constructor(
+    private readonly _queryBus: QueryBus,
+    private readonly _commandBus: CommandBus,
+  ) {}
 
   @Get()
   @HttpCode(HttpStatus.OK)
@@ -36,5 +43,11 @@ export class PostController {
   @HttpCode(HttpStatus.OK)
   async find(@Param('id', ParseIntPipe) id: number): Promise<PostDto> {
     return await this._queryBus.execute(new GetPostByIdQuery(id));
+  }
+
+  @Post('add')
+  @HttpCode(204)
+  async add(@Body() addPostDto: AddPostDto): Promise<number> {
+    return await this._commandBus.execute(new AddPostCommand(addPostDto));
   }
 }
--- src/api/v1/post/post.module.ts
+++ src/api/v1/post/post.module.ts
@@ -4,11 +4,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostController } from './post.controller';
 import { GetPostsHandler } from './queries/handlers/getPostsHandler/get-posts.handler';
 import { PostEntity } from 'src/domain/entities/post.entity';
-import { GetPostByIdHandler } from './queries/handlers/getPostsHandler/get-post-byId.handler';
+import { GetPostByIdHandler } from './queries/handlers/getPostByIdHandler/get-post-byId.handler';
+import { AddPostHandler } from './commands/handlers/add-post.handler';
 
 @Module({
   imports: [CqrsModule, TypeOrmModule.forFeature([PostEntity])],
   controllers: [PostController],
-  providers: [GetPostsHandler, GetPostByIdHandler],
+  providers: [GetPostsHandler, GetPostByIdHandler, AddPostHandler],
 })
 export class PostModule {}
